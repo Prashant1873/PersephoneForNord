3dcfc8b95f58366a6d53c8f3747be0e240db0aa6
LibXZR <i@xzr.moe>
2022-03-03 19:11:40 +0530

    touchpanel_common_driver: Restore game mode status after switching to…

--- drivers/oneplus/input/touchscreen/touchpanel_common_driver.c
+++ drivers/oneplus/input/touchscreen/touchpanel_common_driver.c
@@ -5693,6 +5693,8 @@ static int tfb_notifier_callback(struct notifier_block *self, unsigned long even
 				mutex_lock(&ts->mutex);
 				if (!ts->is_suspended && (ts->suspend_state == TP_SPEEDUP_RESUME_COMPLETE)) {
 					ts->ts_ops->mode_switch(ts->chip_data, MODE_REFRESH_SWITCH, 0);
+					if (ts->noise_level)
+						ts->ts_ops->mode_switch(ts->chip_data, MODE_GAME, true);
 				}
 				mutex_unlock(&ts->mutex);
 			}
@@ -5701,6 +5703,8 @@ static int tfb_notifier_callback(struct notifier_block *self, unsigned long even
 				mutex_lock(&ts->mutex);
 				if (!ts->is_suspended && (ts->suspend_state == TP_SPEEDUP_RESUME_COMPLETE)) {
 					ts->ts_ops->mode_switch(ts->chip_data, MODE_REFRESH_SWITCH, 1);
+					if (ts->noise_level)
+						ts->ts_ops->mode_switch(ts->chip_data, MODE_GAME, true);
 				}
 				mutex_unlock(&ts->mutex);
 			}
@@ -5709,6 +5713,8 @@ static int tfb_notifier_callback(struct notifier_block *self, unsigned long even
 				mutex_lock(&ts->mutex);
 				if (!ts->is_suspended && (ts->suspend_state == TP_SPEEDUP_RESUME_COMPLETE)) {
 					ts->ts_ops->mode_switch(ts->chip_data, MODE_REFRESH_SWITCH, 1);
+					if (ts->noise_level)
+						ts->ts_ops->mode_switch(ts->chip_data, MODE_GAME, true);
 				}
 				mutex_unlock(&ts->mutex);
 			}
