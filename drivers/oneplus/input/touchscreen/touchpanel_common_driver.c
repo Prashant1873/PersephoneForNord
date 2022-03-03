01aa8f234e046430dcaef2c9b3439cdfd55c6846
LibXZR <i@xzr.moe>
2022-03-03 19:43:35 +0530

    [SQUASH] touchscreen: Optimize interrupt CPU usage & Adapt PM QoS usage to incoherent OnePlus code
    * Adapt from kerneltoast/android_kernel_google_floral@5726443
    * This also reduces input latency
    Since OnePlus frees the IRQ, we need to remove the PM QoS request prior
    to that in order to unregister the IRQ affinity notifier in the PM QoS
    request. We also need to move the pm_qos_add_request() operation to
    come before the IRQ is registered, to prevent the IRQ handler from using
    the PM QoS structures while they're uninitialized.

--- drivers/oneplus/input/touchscreen/touchpanel_common_driver.c
+++ drivers/oneplus/input/touchscreen/touchpanel_common_driver.c
@@ -942,6 +942,10 @@ static void tp_work_func(struct touchpanel_data *ts)
 		TPD_INFO("not support ts_ops->trigger_reason callback\n");
 		return;
 	}
+
+	pm_qos_update_request(&ts->pm_touch_req, 100);
+	pm_qos_update_request(&ts->pm_i2c_req, 100);
+
 	/*
 	 *  trigger_reason:this callback determine which trigger reason should be
 	 *  The value returned has some policy!
@@ -991,6 +995,9 @@ static void tp_work_func(struct touchpanel_data *ts)
 	} else {
 		TPD_DEBUG("unknown irq trigger reason\n");
 	}
+
+	pm_qos_update_request(&ts->pm_i2c_req, PM_QOS_DEFAULT_VALUE);
+	pm_qos_update_request(&ts->pm_touch_req, PM_QOS_DEFAULT_VALUE);
 }
 
 static void tp_work_func_unlock(struct touchpanel_data *ts)
@@ -4740,6 +4747,16 @@ int tp_register_irq_func(struct touchpanel_data *ts)
 {
 	int ret = 0;
 
+	ts->pm_i2c_req.type = PM_QOS_REQ_AFFINE_IRQ;
+	ts->pm_i2c_req.irq = geni_i2c_get_adap_irq(ts->client);
+	pm_qos_add_request(&ts->pm_i2c_req, PM_QOS_CPU_DMA_LATENCY,
+			   PM_QOS_DEFAULT_VALUE);
+
+	ts->pm_touch_req.type = PM_QOS_REQ_AFFINE_IRQ;
+	ts->pm_touch_req.irq = ts->irq;
+	pm_qos_add_request(&ts->pm_touch_req, PM_QOS_CPU_DMA_LATENCY,
+			   PM_QOS_DEFAULT_VALUE);
+
 #ifdef TPD_USE_EINT
 	if (gpio_is_valid(ts->hw_res.irq_gpio)) {
 		TPD_DEBUG("%s, irq_gpio is %d, ts->irq is %d\n", __func__, ts->hw_res.irq_gpio, ts->irq);
@@ -4749,9 +4766,11 @@ int tp_register_irq_func(struct touchpanel_data *ts)
 				TPD_DEVICE, ts);
 		if (ret < 0) {
 			TPD_INFO("%s request_threaded_irq ret is %d\n", __func__, ret);
+			goto err_irq;
 		}
 	} else {
 		TPD_INFO("%s:no valid irq\n", __func__);
+		goto err_irq;
 	}
 #else
 	hrtimer_init(&ts->timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
@@ -4759,7 +4778,19 @@ int tp_register_irq_func(struct touchpanel_data *ts)
 	hrtimer_start(&ts->timer, ktime_set(3, 0), HRTIMER_MODE_REL);
 #endif
 
+	return 0;
+
+err_irq:
+	pm_qos_remove_request(&ts->pm_touch_req);
+	pm_qos_remove_request(&ts->pm_i2c_req);
 	return ret;
+}
+
+static void tp_unregister_irq_func(struct touchpanel_data *ts)
+{
+	pm_qos_remove_request(&ts->pm_touch_req);
+	pm_qos_remove_request(&ts->pm_i2c_req);
+	free_irq(ts->irq, ts);
 }
 
 //work schdule for reading&update delta
@@ -5270,7 +5301,7 @@ earsense_alloc_free:
 	kfree(ts->earsense_delta);
 
 threaded_irq_free:
-	free_irq(ts->irq, ts);
+	tp_unregister_irq_func(ts);
 
 manu_info_alloc_err:
 	kfree(ts->panel_data.manufacture_info.version);
@@ -5286,7 +5317,7 @@ free_touch_panel_input:
 
 err_check_functionality_failed:
 	if (ts->int_mode == UNBANNABLE) {
-		free_irq(ts->irq, ts);
+		tp_unregister_irq_func(ts);
 	}
 	//ts->ts_ops->power_control(ts->chip_data, false);
 
@@ -5457,7 +5488,7 @@ static void tp_resume(struct device *dev)
 		goto NO_NEED_RESUME;
 
 	//free irq at first
-	free_irq(ts->irq, ts);
+	tp_unregister_irq_func(ts);
 
 	if (ts->ts_ops->reinit_device) {
 		ts->ts_ops->reinit_device(ts->chip_data);
