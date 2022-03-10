25b2b9bc369f6cb6e1a5ca800323cc449ef57381
Prashant1873 <shuklaprashant847@gmail.com>
2022-03-10 22:22:32 +0530

    Misc

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -103,8 +103,8 @@ walt_dec_cfs_rq_stats(struct cfs_rq *cfs_rq, struct task_struct *p) {}
  *
  * (default: 6ms * (1 + ilog(ncpus)), units: nanoseconds)
  */
-unsigned int sysctl_sched_latency			= 800000ULL;
-unsigned int normalized_sysctl_sched_latency		= 800000ULL;
+unsigned int sysctl_sched_latency			= 2000000ULL;
+unsigned int normalized_sysctl_sched_latency		= 2000000ULL;
 
 /*
  * Enable/disable honoring sync flag in energy-aware wakeups.
@@ -135,26 +135,26 @@ extern void  update_stuck_trace_info(struct task_struct *tsk, int trace_type, un
  *
  * (default SCHED_TUNABLESCALING_LOG = *(1+ilog(ncpus))
  */
-enum sched_tunable_scaling sysctl_sched_tunable_scaling = SCHED_TUNABLESCALING_NONE;
+enum sched_tunable_scaling sysctl_sched_tunable_scaling = SCHED_TUNABLESCALING_LOG;
 
 /*
  * Minimal preemption granularity for CPU-bound tasks:
  *
  * (default: 0.75 msec * (1 + ilog(ncpus)), units: nanoseconds)
  */
-unsigned int sysctl_sched_min_granularity		= 100000ULL;
-unsigned int normalized_sysctl_sched_min_granularity	= 100000ULL;
+unsigned int sysctl_sched_min_granularity		= 200000ULL;
+unsigned int normalized_sysctl_sched_min_granularity	= 200000ULL;
 
 /*
  * This value is kept at sysctl_sched_latency/sysctl_sched_min_granularity
  */
-static unsigned int sched_nr_latency = 8;
+static unsigned int sched_nr_latency = 10;
 
 /*
  * After fork, child runs first. If set to 0 (default) then
  * parent will (try to) run first.
  */
-unsigned int sysctl_sched_child_runs_first __read_mostly;
+unsigned int sysctl_sched_child_runs_first __read_mostly = 1;
 
 /*
  * SCHED_OTHER wake-up granularity.
@@ -165,8 +165,8 @@ unsigned int sysctl_sched_child_runs_first __read_mostly;
  *
  * (default: 1 msec * (1 + ilog(ncpus)), units: nanoseconds)
  */
-unsigned int sysctl_sched_wakeup_granularity		= 200000UL;
-unsigned int normalized_sysctl_sched_wakeup_granularity	= 200000UL;
+unsigned int sysctl_sched_wakeup_granularity		= 500000UL;
+unsigned int normalized_sysctl_sched_wakeup_granularity	= 500000UL;
 
 const_debug unsigned int sysctl_sched_migration_cost	= 500000UL;
 DEFINE_PER_CPU_READ_MOSTLY(int, sched_load_boost);
